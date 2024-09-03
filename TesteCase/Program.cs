cae4fc781663ba047bd1ea0225eb890343d5b5ae
Queiroz1992 <51212831+Queiroz1992@users.noreply.github.com>
2024-09-03 12:17:28 -0300

    Teste de lógica, SubArraySum

---
+++ TesteCase/Program.cs
@@ -0,0 +1,12 @@
+﻿using TesteCase;
+
+public class Program
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.CheckSubarraySum(new int[] { 23, 2, 4, 6, 7 }, 6));  
+        Console.WriteLine(solution.CheckSubarraySum(new int[] { 23, 2, 6, 4, 7 }, 6));  
+        Console.WriteLine(solution.CheckSubarraySum(new int[] { 23, 2, 6, 4, 7 }, 13)); 
+    }
+}
---
+++ TesteCase/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteCase
+{
+    public class Solution
+    {
+        public bool CheckSubarraySum(int[] nums, int k)
+        {
+            // Dicionário para armazenar o resto da soma acumulada e o índice correspondente
+            Dictionary<int, int> remainderDict = new Dictionary<int, int>();
+            remainderDict[0] = -1;
+            int cumulativeSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                cumulativeSum += nums[i];
+                int remainder = cumulativeSum % k;
+
+                if (remainderDict.ContainsKey(remainder))
+                {
+                    if (i - remainderDict[remainder] > 1)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    remainderDict[remainder] = i;
+                }
+            }
+
+            return false;
+        }
+    }
+}
